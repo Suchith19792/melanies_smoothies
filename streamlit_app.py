620b6faa9f3f89a2a48ef302782ef0d8ba26d65b
Suchith Shetty <56347999+Suchith19792@users.noreply.github.com>
2026-06-11 19:48:52 +0100

    Update API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list:
         st.success(success_stmt, icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
