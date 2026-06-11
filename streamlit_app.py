ae93602db11ce26427bee1ba1c9dea0a1b527839
Suchith Shetty <56347999+Suchith19792@users.noreply.github.com>
2026-06-11 20:24:31 +0100

    df to pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import requests
 import streamlit as st
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -18,7 +19,8 @@ st.write('Name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(data=pd_df, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
